3ba878004b70eda345da592a992d91bdab967862
Aman Yadav <127370497+Amnyadav@users.noreply.github.com>
2025-06-02 00:59:55 +0530

    Update apis.js
    url update

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 const REACT_APP_BASE_URL =
-  process.env.REACT_APP_BASE_URL || "edtech-one-steel.vercel.app";
+  process.env.REACT_APP_BASE_URL || "edtech-one-steel.vercel.app/api/v1";
 const url=process.env.REACT_APP_BASE_URL;
 console.log("tthese is url from env file",url);
 
